9039be5ed19e32d719447d2b4d222166488c2bbb
Reese Kunz <reesekunz21@gmail.com>
2019-10-29 14:36:49 -0700

    merge commit

--- services/jsonCache.js
+++ services/jsonCache.js
@@ -1,131 +1,117 @@
-const fs = require("fs")
-const axios = require("axios")
-const prismic = require("./prismicData")
-const moment = require("moment")
+const fs = require("fs");
+const axios = require("axios");
+const prismic = require("./prismicData");
+const moment = require("moment");
 
 async function cacheResource(resourceName, resourceLoader) {
-  const resource = await resourceLoader()
-  const json = JSON.stringify(resource)
-  const path = __dirname + "/../assets/cache/" + resourceName + ".json"
+  const resource = await resourceLoader();
+  const json = JSON.stringify(resource);
+  const path = __dirname + "/../assets/cache/" + resourceName + ".json";
   return new Promise((resolve, reject) => {
     fs.writeFile(path, json, err => {
       if (err) {
-        reject(err)
+        reject(err);
       } else {
-        resolve()
+        resolve();
       }
-    })
-  })
+    });
+  });
 }
 
 async function main() {
   try {
-    console.log("Fetching Init")
-    await cacheResource("pumps", getPumps)
-    await cacheResource("longStore", createStore)
-    process.exit(0)
+    console.log("Fetching Init");
+    await cacheResource("pumps", getPumps);
+    await cacheResource("longStore", createStore);
+    process.exit(0);
   } catch (err) {
-    console.error(err)
-    process.exit(1)
+    console.error(err);
+    process.exit(1);
   }
 }
 
 const url =
-  "https://dashboard.welldone.org/.netlify/functions/get_momo_status?id="
+  "https://dashboard.welldone.org/.netlify/functions/get_momo_status?id=";
 async function getPumps() {
   // const { data: config } = await prismic.getDoc("config")
   // we don't want to subtract this data anymore...
   // const oldData = require("../assets/cache/pumps.json")
   // if (
-    // // ! oldData.lastFetch ||
+  // // ! oldData.lastFetch ||
   //   oldData.lastFetch <
   //     moment()
   //       .subtract(config.update_data, "hours")
   //       .unix()
   // ) {
   //   console.log("Fetching Pumps Init")
-    let pumps = {}
-    const prismicPumps = await prismic.getDocs("pump")
-    await asyncForEach(prismicPumps.results, async pump => {
-      let village = null
-      if (pump.data && pump.data.village.id && !pump.data.village.isBroken) {
-        village = await prismic.getVillage(pump.data.village.id)
-      }
-      if (pump.data && pump.data.latitude && pump.data.longitude) {
-        pumps = {
-          ...pumps,
-          [pump.uid]: {
-            ...pump.data,
-            village,
-          },
+  let pumps = {};
+  const prismicPumps = await prismic.getDocs("pump");
+  await asyncForEach(prismicPumps.results, async pump => {
+    let village = null;
+    if (pump.data && pump.data.village.id && !pump.data.village.isBroken) {
+      village = await prismic.getVillage(pump.data.village.id);
+    }
+    if (pump.data && pump.data.latitude && pump.data.longitude) {
+      pumps = {
+        ...pumps,
+        [pump.uid]: {
+          ...pump.data,
+          village
         }
-      } else {
-        console.log(`Missing data on pump #${pump.uid}`)
-      }
-    })
+      };
+    } else {
+      console.log(`Missing data on pump #${pump.uid}`);
+    }
+  });
 
-    let results = []
-    await asyncForEach(Object.keys(pumps), async (pump, index) => {
-      try {
-        console.log(`${index + 1}/${Object.keys(pumps).length}`)
-        const res = await axios.get(`${url}${pump}`)
-        let newData = {}
-        res.data
-          ? res.data.dates.forEach((date, index) => {
-              newData = {
-                ...newData,
-                statuses: {
-                  date: date,
-                  count: res.data.statuses[index].count,
-                  total: res.data.statuses[index].total,
-                  status: res.data.statuses[index].status,
-                  pad_counts: res.data.statuses[index].padCounts,
-                  pad_seconds:res.data.statuses[index].padSeconds,
-                  reported_percent:res.data.statuses[index].reportedPercent
-                },
+  let results = [];
+  await asyncForEach(Object.keys(pumps), async (pump, index) => {
+    try {
+      console.log(`${index + 1}/${Object.keys(pumps).length}`);
+      const res = await axios.get(`${url}${pump}`);
+      let newData = {};
+      res.data
+        ? res.data.dates.forEach((date, index) => {
+            newData = {
+              ...newData,
+              statuses: {
+                date: date,
+                count: res.data.statuses[index].count,
+                total: res.data.statuses[index].total,
+                status: res.data.statuses[index].status,
+                pad_counts: res.data.statuses[index].padCounts,
+                pad_seconds: res.data.statuses[index].padSeconds,
+                reported_percent: res.data.statuses[index].reportedPercent
               }
-            })
-            : []
-          // : statuses = {
-          //     date: date,
-          //     count: 0,
-          //     total: 0,
-          //     status: 0,
-          //     pad_counts: 0,
-          //     pad_seconds: 0,
-          //     reportedPercent: 0
-          //  }
-          console.log("results line 98", results)
-        results.push({
-          id: pump,
-          ...pumps[pump],
-          status: res.data.status,
-          statuses: newData,
-        })
-      } catch (err) {
-        console.error(`Error on pump #${pump}`)
-        results.push({ id: pump, 
-          ...pumps[pump], 
-          status: 0,
-          dates: 0, 
-          statuses: 0, 
-          error: "500" })
-      }
-    })
-    // console.log(newData, 'this is the new data')
-    console.log("Fetching Pumps Success")
-    return { lastFetch: moment().unix(), pumps: results }
-  } 
-  // else { 
-  //   console.log("Data Up To Date")
-  //   return oldData
-  // }
+            };
+          })
+        : {};
+      // : statuses = {
 
+      // }
+      results.push({
+        id: pump,
+        ...pumps[pump],
+        status: res.data.status,
+        statuses: newData
+      });
+    } catch (err) {
+      console.error(`Error on pump #${pump}`);
+      results.push({ id: pump, ...pumps[pump], status: 0, error: "500" });
+    }
+  });
+  console.log("Fetching Pumps Success");
+  return { lastFetch: moment().unix(), pumps: results };
+}
+// else {
+//   console.log("Data Up To Date")
+//   return oldData
+// }
 
 async function createStore() {
   // const oldData = require("../assets/cache/longStore.json")
-  const data = require("../assets/cache/pumps.json")
-  let pumps = {}
+  const data = require("../assets/cache/pumps.json");
+  let pumps = {};
   data.pumps.forEach(({ id, dates, statuses }, index) => {
     // let pumpOldData = oldData.pumps ? oldData.pumps[id] : {}
 
@@ -133,19 +119,19 @@ async function createStore() {
       ...pumps,
       [id]: {
         // ...pumpOldData,
-        ...data.pumps.find(pump => pump.id === id).statuses,
-      },
+        ...data.pumps.find(pump => pump.id === id).statuses
+      }
       // statuses: 0
-    }
-  })
+    };
+  });
 
-  return { pumps }
+  return { pumps };
 }
 
 async function asyncForEach(array, callback) {
   for (let index = 0; index < array.length; index++) {
-    await callback(array[index], index, array)
+    await callback(array[index], index, array);
   }
 }
-     
+
 main();
